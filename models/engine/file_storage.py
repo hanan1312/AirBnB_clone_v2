c23ed4ee67a3b6f1673abe35df40d29b1a51f94e
hanan1312 <nana.elmogey@gmail.com>
2024-05-19 10:03:49 +0300

    web flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,7 +49,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -57,5 +57,9 @@ class FileStorage:
         """Deletes obj from __objects if it's inside"""
         if obj is not None:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
+
+    def close(self):
+        """Call reload() method for deserializing the JSON file to objects"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.city import City
-
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
@@ -12,13 +12,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete-orphan", backref="state")
 
-    @property
-    def cities(self):
-        """Getter attribute for cities"""
-        from models import storage
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
-
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter attribute for cities when storage engine is not DBStorage"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
